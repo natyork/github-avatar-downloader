3476473a03466fffb37c5f9d7b6dbffc31d94f14
Nat York <natyork@gmail.com>
2016-10-10 23:42:39 +0000

    midway commit in case I mess up and delete everything

--- get-repo-contributors.js
+++ get-repo-contributors.js
@@ -1,31 +1,61 @@
 var request = require("request");
+var fs = require("fs");
 
 function getRepoContributors(repoOwner, repoName, cb) { // convention for asynchronous callback func
   const apiEndPoint = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors"
+  console.log(apiEndPoint);
+  var options = {
+  url: apiEndPoint,
+  method: 'GET',
+  headers: {
+    'User-Agent': 'request'
+  },
+  json: true
+};
 
-
-
+  cb(options);
 }
 
-function readHTML (url, print) {
+// function readInfo (url) {
+//   request(url, function(err, response, body) {
+//     if (err) {
+//       throw err;
+//     }
+//   console.log(body);
+//   });
+// }
+
+
+
+function downloadImageByURL(url, filePath) {
   request(url, function(err, response, body) {
     if (err) {
       throw err;
     }
-  print(body);
+
+    var avatarArray = body.map(function (element, index, array){
+      var fileName = element.login + ".jpg"
+      var fileContent = request(element.avatar_url, function(err, response, body) {
+        if (err) {
+          throw err;
+        }
+        fs.writeFile(fileName, fileContent,  function(err) {
+          if (err) {
+            return console.error(err);
+          }
+          console.log("wrote to:" + fileName);
+        });
+    });
+  console.log(avatarArray);
+  //do another rquest to each url in the newarray
   });
+});
 }
 
-function printHTML (htmlData){
-  console.log(htmlData);
-}
 
-readHTML("http://www.example.com", printHTML)
 
 // test
-getRepoContributors("lighthouse-labs", "laser_shark", (err, result) => {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
+getRepoContributors("lighthouse-labs", "laser_shark", downloadImageByURL);
+
 
 
